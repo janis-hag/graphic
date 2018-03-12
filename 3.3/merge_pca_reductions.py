962d8afe33a8bd137cf37441b705dd604a7a17b1
AnthonyCheetham <anthony.cheetham@unige.ch>
2018-03-12 10:07:19 +0100

    Added script to merge multiple PCA reductions for NACO-ISPY

---
+++ 3.3/merge_pca_reductions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 20 11:31:53 2017
+
+@author: emilyrickman
+"""
+from astropy.io import fits
+import numpy as np
+import os
+import glob
+
+basedirectory = "./GRAPHIC_PCA/"
+
+files = glob.glob(basedirectory + "*/*_derot.fits")
+
+files = np.sort(files)
+
+cube = []
+
+for x,filename in enumerate(files):
+    image, header = fits.getdata(filename, header = True)
+    cube.append(image)
+    if x==0:
+        outputhdr=header
+
+    outputhdr['HIERARCH GC PCA FRAME' + str(x)] = header['HIERARCH GC PCA ']
+    outputhdr['HIERARCH GC PCA FRAME' + str(x)+' NMODES'+] = header['HIERARCH GC PCA NMODES']
+    outputhdr['HIERARCH GC PCA FRAME' + str(x)+' MINREFFRAMES'] = header['HIERARCH GC PCA MINREFFRAMES']
+    
+fits.writeto(basedirectory + 'pca_multimodes.fits',cube,outputhdr,clobber=True)
