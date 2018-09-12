4222b523daad26bd3f655739d0589bd1a065455c
Janis Hagelberg <astro.janis@gmail.com>
2018-09-12 15:14:25 +0200

    Debugging master_flat

--- 3.3/master_flat_sphere.py
+++ 3.3/master_flat_sphere.py
@@ -27,7 +27,7 @@ if rank==0:
 		hdr_masterflat = hdulist.header
 		hdr_masterflat['Routine used'] = "master_flat_func routine from Sebastien P"  # Add a new keyword
 		for allfiles in glob.iglob(key_word+'*'):
-			print allfiles
+			print(allfiles)
 			hdr_masterflat['flat nbr '+np.str(count+1)+' used'] = allfiles  # Add a new keyword
 			if count==0:
 				temp,hdr=pyfits.getdata(allfiles, header=True)
