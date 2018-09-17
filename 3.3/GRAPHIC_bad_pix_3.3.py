f9c11f0ac18832e08bd653fbf0cc2fd9b11ed745
Janis Hagelberg <astro.janis@gmail.com>
2018-09-17 16:56:58 +0200

    Converted 3.3/GRAPHIC_bad_pix_3.3.py to python3

--- 3.3/GRAPHIC_bad_pix_3.3.py
+++ 3.3/GRAPHIC_bad_pix_3.3.py
@@ -100,23 +100,25 @@ def gen_badpix(sky, coef, comments, cut):
     global median
 
     med = nanmedian(sky)
-    sigma = 1.4826*nanmedian(np.abs(sky-med)) # the 1.4826 converts the Median Absolute Deviation to the standard deviation for a Normal distribution
+    sigma = 1.4826*nanmedian(np.abs(sky-med))
+    # the 1.4826 converts the Median Absolute Deviation
+    # to the standard deviation for a Normal distribution
 
     print("Sigma: "+str(sigma)+", median: "+str(med))
 
-    #Creates a tuple with the x-y positions of the dead pixels
-    deadpix=np.where(sky < med-sigma*coef )
+    # Creates a tuple with the x-y positions of the dead pixels
+    deadpix = np.where(sky < med-sigma*coef)
 
-    #Creates a tuple with the x-y positions of the dead pixels
-    hotpix=np.where(sky > med+sigma*coef )
+    # Creates a tuple with the x-y positions of the dead pixels
+    hotpix = np.where(sky > med+sigma*coef)
 
-    #negativepix=np.where(sky < 0 )
+    # negativepix=np.where(sky < 0 )
 
     # Warning! x and y inverted with respect to ds9
-    dprint(d>2, "sky.shape: "+str(sky.shape)+", sky.size: "+str(sky.size))
+    dprint(d > 2, "sky.shape: " + str(sky.shape) + ", sky.size: " + str(sky.size))
     # if np.shape(deadpix)[1]+np.shape(negativepix)[1]+np.shape(hotpix)[1]==0:
-    if np.shape(deadpix)[1]+np.shape(hotpix)[1]==0:
-        # This used to abort and leave all procs running at full speed indefinitely.
+    if np.shape(deadpix)[1] + np.shape(hotpix)[1] == 0:
+        # This used to abort and leave all procs running in infinite loops.
         # But in principal this could legitimately happen, so ACC removed this section
         # c="No bad pixels found. Aborting"
         # dprint(d>2, 'No bad pixels found. Aborting!')
@@ -127,183 +129,209 @@ def gen_badpix(sky, coef, comments, cut):
         # sys.exit(1)
 
         # Instead, how about we just return an empty list
-        c="No badpix found!"
+        c = "No badpix found!"
         comments.append(c)
         print("WARNING: No bad pixels were found!")
     else:
-        c="Found "+str(np.shape(deadpix)[1])+" = "+str(100.*np.shape(deadpix)[1]/sky.size)+"% dead, "+\
-        "and "+str(np.shape(hotpix)[1])+" = "+str(100.*np.shape(hotpix)[1]/sky.size)+"% hot pixels."
-        # print(c)
+        c = "Found "+str(np.shape(deadpix)[1]) + " = " + str(100.*np.shape(deadpix)[1]/sky.size)+"% dead, " + "and "+str(np.shape(hotpix)[1]) + " = " + str(100.*np.shape(hotpix)[1]/sky.size) + "% hot pixels."
         comments.append(c)
 
-
-    badpix=tuple(np.append(np.array(deadpix),np.array(hotpix), axis=1))
-    ## badpix=tuple(np.append(np.array(badpix),np.array(negativepix), axis=1))
+    badpix = tuple(np.append(np.array(deadpix), np.array(hotpix), axis=1))
 
     return badpix, comments
-    #    return deadpix, hotpix, negativepix
 
 
 def clean_bp(badpix, cub_in):
     global nanmedian
-    #(deadpix, hotpix, negativepix, cub_in):
-    cub_in[:,badpix[0],badpix[1]]=np.NaN
+    # (deadpix, hotpix, negativepix, cub_in):
+    cub_in[:, badpix[0], badpix[1]] = np.NaN
     for f in range(cub_in.shape[0]):
         if args.interactive:
             sys.stdout.write('\r Frame '+str(f+1)+' of '+str(cub_in.shape[0]))
             sys.stdout.flush()
 
         for j in range(len(badpix[0])):
-            y=badpix[0][j]
-            x=badpix[1][j]
-            ## print(j,y,x)
-            if y == cube.shape[1]-1: # At the image edge !!!
-                if x == cube.shape[2]-1: # In a corner
-                    cub_in[f,y,x]=nanmedian([cub_in[f,y-1,x-1],cub_in[f,y-1,x],
-                                                cub_in[f,y,x-1]])
+            y = badpix[0][j]
+            x = badpix[1][j]
+
+            if y == cube.shape[1]-1:  # At the image edge !!!
+                if x == cube.shape[2]-1:  # In a corner
+                    cub_in[f, y, x] = nanmedian([cub_in[f, y-1, x-1],
+                                                cub_in[f, y-1, x],
+                                                cub_in[f, y, x-1]])
                 elif x == 0:
-                    cub_in[f,y,x]=nanmedian([cub_in[f,y-1,x],cub_in[f,y-1,x+1],
-                                                cub_in[f,y,x+1]])
-                else: # Along the edge
-                    cub_in[f,y,x]=nanmedian([cub_in[f,y-1,x-1],cub_in[f,y-1,x],cub_in[f,y-1,x+1],
-                                                cub_in[f,y,x-1],cub_in[f,y,x+1]])
-            elif y == 0: # At the image edge !!!
-                if x == cube.shape[2]-1: # In a corner
-                    cub_in[f,y,x]=nanmedian([cub_in[f,y,x-1],
-                                                cub_in[f,y+1,x-1],cub_in[f,y+1,x]])
+                    cub_in[f, y, x] = nanmedian([cub_in[f, y-1, x],
+                                                cub_in[f, y-1, x+1],
+                                                cub_in[f, y, x+1]])
+                else:  # Along the edge
+                    cub_in[f, y, x] = nanmedian([cub_in[f, y-1, x-1],
+                                                cub_in[f, y-1, x],
+                                                cub_in[f, y-1, x+1],
+                                                cub_in[f, y, x-1],
+                                                cub_in[f, y, x+1]])
+            elif y == 0:  # At the image edge !!!
+                if x == cube.shape[2]-1:  # In a corner
+                    cub_in[f, y, x] = nanmedian([cub_in[f, y, x-1],
+                                                cub_in[f, y+1, x-1],
+                                                cub_in[f, y+1, x]])
                 elif x == 0:  # In a corner
-                    cub_in[f,y,x]=nanmedian([cub_in[f,y,x+1],
-                                                cub_in[f,y+1,x],cub_in[f,y+1,x+1]])
-                else: # Along the edge
-                    cub_in[f,y,x]=nanmedian([cub_in[f,y,x-1],cub_in[f,y,x+1],
-                                                cub_in[f,y+1,x-1],cub_in[f,y+1,x],cub_in[f,y+1,x+1]])
-            elif x == 0: # Along the edge
-                cub_in[f,y,x]=nanmedian([cub_in[f,y-1,x],
-                                                cub_in[f,y-1,x+1],cub_in[f,y,x+1],
-                                                cub_in[f,y+1,x],
-                                                cub_in[f,y+1,x+1]])
-            elif x == cub_in.shape[2]-1: # Along the edge
-                cub_in[f,y,x]=nanmedian([cub_in[f,y-1,x-1],cub_in[f,y-1,x],
-                                          cub_in[f,y,x-1],
-                                          cub_in[f,y+1,x-1],cub_in[f,y+1,x]])
-            else: # Usual case, not on an edge
-                cub_in[f,y,x]=nanmedian([cub_in[f,y-1,x-1],cub_in[f,y-1,x],cub_in[f,y-1,x+1],
-                                            cub_in[f,y,x-1],cub_in[f,y,x+1],
-                                            cub_in[f,y+1,x-1],cub_in[f,y+1,x],cub_in[f,y+1,x+1]])
+                    cub_in[f, y, x] = nanmedian([cub_in[f, y, x+1],
+                                                cub_in[f, y+1, x],
+                                                cub_in[f, y+1, x+1]])
+                else:  # Along the edge
+                    cub_in[f, y, x] = nanmedian([cub_in[f, y, x-1],
+                                                cub_in[f, y, x+1],
+                                                cub_in[f, y+1, x-1],
+                                                cub_in[f, y+1, x],
+                                                cub_in[f, y+1, x+1]])
+            elif x == 0:  # Along the edge
+                cub_in[f, y, x] = nanmedian([cub_in[f, y-1, x],
+                                            cub_in[f, y-1, x+1],
+                                            cub_in[f, y, x+1],
+                                            cub_in[f, y+1, x],
+                                            cub_in[f, y+1, x+1]])
+            elif x == cub_in.shape[2]-1:  # Along the edge
+                cub_in[f, y, x] = nanmedian([cub_in[f, y-1, x-1],
+                                            cub_in[f, y-1, x],
+                                            cub_in[f, y, x-1],
+                                            cub_in[f, y+1, x-1],
+                                            cub_in[f, y+1, x]])
+            else:  # Usual case, not on an edge
+                cub_in[f, y, x] = nanmedian([cub_in[f, y-1, x-1],
+                                            cub_in[f, y-1, x],
+                                            cub_in[f, y-1, x+1],
+                                            cub_in[f, y, x-1],
+                                            cub_in[f, y, x+1],
+                                            cub_in[f, y+1, x-1],
+                                            cub_in[f, y+1, x],
+                                            cub_in[f, y+1, x+1]])
     return cub_in
 
-t_init=MPI.Wtime()
-if sphere: # for SPHERE a large part of the image are badpix because outside of the filter. We apply a nan mask to the dark so there are not determined as bad pixels and therefore makes the code much faster. This part of the image is set to nan for the science cubes as well
-    #applying nan mask on the image part where there is no filter
-    x=np.arange(1024)
-    y=np.copy(x)
-    X,Y=np.meshgrid(x,y)
-    z=np.arange(-512,512)
-    centre_filtre_left=[471,529]
-    centre_filtre_right=[1558-1024,516]
-    w=np.copy(z)
-    Z,W=np.meshgrid(z,w)
-    R_left=np.sqrt((Z+(512-centre_filtre_left[0]))**2+(W+(512-centre_filtre_left[1]))**2)
-    R_right=np.sqrt((Z+(512-centre_filtre_right[0]))**2+(W+(512-centre_filtre_right[1]))**2)
-    R=np.sqrt(Z**2+W**2)
-    #left part of the image
-    mask_nan_l=np.where(X<50,np.nan,1.)
-    mask_nan_l=np.where(X>934,np.nan,mask_nan_l)
-    mask_nan_l=np.where(Y<22,np.nan,mask_nan_l)
-    mask_nan_l=np.where(R>521,np.nan,mask_nan_l)
-    #right image
-    mask_nan_r=np.where(X<55,np.nan,1.)
-    mask_nan_r=np.where(X>935,np.nan,mask_nan_r)
-    mask_nan_r=np.where(Y<12,np.nan,mask_nan_r)
-    mask_nan_r=np.where(Y>1014,np.nan,mask_nan_r)
-    mask_nan_r=np.where(R>531,np.nan,mask_nan_r)
-    mask_nan=np.append(mask_nan_l,mask_nan_r,axis=1)
 
-if rank==0:
+t_init=MPI.Wtime()
+
+if sphere:
+    # for SPHERE a large part of the image are badpix because outside of the
+    # filter. We apply a nan mask to the dark so there are not determined as
+    # bad pixels and therefore makes the code much faster. This part of the
+    # image is set to nan for the science cubes as well
+    # applying nan mask on the image part where there is no filter
+
+    x = np.arange(1024)
+    y = np.copy(x)
+    X, Y = np.meshgrid(x, y)
+    z = np.arange(-512, 512)
+    centre_filtre_left = [471, 529]
+    centre_filtre_right = [1558-1024, 516]
+    w = np.copy(z)
+    Z, W = np.meshgrid(z, w)
+    R_left = np.sqrt((Z + (512-centre_filtre_left[0]))**2
+                     + (W + (512-centre_filtre_left[1]))**2)
+    R_right = np.sqrt((Z + (512-centre_filtre_right[0]))**2
+                      + (W + (512-centre_filtre_right[1]))**2)
+    R = np.sqrt(Z**2 + W**2)
+    #left part of the image
+    mask_nan_l = np.where(X < 50, np.nan, 1.)
+    mask_nan_l = np.where(X > 934, np.nan, mask_nan_l)
+    mask_nan_l = np.where(Y < 22, np.nan, mask_nan_l)
+    mask_nan_l = np.where(R > 521, np.nan, mask_nan_l)
+    #right image
+    mask_nan_r = np.where(X < 55, np.nan, 1.)
+    mask_nan_r = np.where(X > 935, np.nan, mask_nan_r)
+    mask_nan_r = np.where(Y < 12, np.nan, mask_nan_r)
+    mask_nan_r = np.where(Y > 1014, np.nan, mask_nan_r)
+    mask_nan_r = np.where(R > 531, np.nan, mask_nan_r)
+    mask_nan = np.append(mask_nan_l, mask_nan_r, axis=1)
+
+if rank == 0:
     graphic_nompi_lib.print_init()
 
-    t_init=MPI.Wtime()
+    t_init = MPI.Wtime()
 
     print("Searching cubes...")
-    dirlist=graphic_nompi_lib.create_dirlist(pattern, target_pattern=target_pattern)
+    dirlist = graphic_nompi_lib.create_dirlist(
+            pattern, target_pattern=target_pattern)
     print("Searching reference cubes...")
-    darklist=graphic_nompi_lib.create_dirlist(dark_dir+os.sep+dark_pattern)
+    darklist = graphic_nompi_lib.create_dirlist(
+            dark_dir + os.sep + dark_pattern)
 
     if dirlist is None or darklist is None:
         print('Missing files, leaving...')
         MPI.Finalize()
         sys.exit(1)
 
-    start,dirlist=graphic_mpi_lib.send_dirlist(dirlist)
-    dprint(d>2, 'Dirlist sent to slaves')
+    start, dirlist = graphic_mpi_lib.send_dirlist(dirlist)
+    dprint(d > 2, 'Dirlist sent to slaves')
 
-    dark_cube=None
+    dark_cube = None
     for file_name in darklist:
-        ## dark_hdulist = fits.open(file_name)
-        ## data=dark_hdulist[0].data
-        data=pyfits.getdata(file_name, header=False)
+        # dark_hdulist = fits.open(file_name)
+        # data=dark_hdulist[0].data
+        data = pyfits.getdata(file_name, header=False)
         if dark_cube is None:
-            dark_cube=data #[np.newaxis,...]
-            ## print(dark_cube.shape)
-        elif len(data.shape)==3: # CUBE
-            ## print(data.shape)
-            ## dark_cube=np.concatenate((dark_cube,data[np.newaxis,...]),axis=0)
-            dark_cube=np.concatenate((dark_cube,data),axis=0)
-            ## print(dark_cube.shape)
-        elif len(data.shape)==2: # FRAME
-            if len(dark_cube.shape)==3:
-                dark_cube=np.rollaxis(dark_cube,0,3)
+            dark_cube = data   #[ np.newaxis,...]
+            # print(dark_cube.shape)
+        elif len(data.shape)==3:  # CUBE
+            # print(data.shape)
+            # dark_cube=np.concatenate((dark_cube,data[np.newaxis,...]),axis=0)
+            dark_cube = np.concatenate((dark_cube, data), axis=0)
+            # print(dark_cube.shape)
+        elif len(data.shape) == 2: # FRAME
+            if len(dark_cube.shape) == 3:
+                dark_cube = np.rollaxis(dark_cube, 0, 3)
             ## print(data.shape, dark_cube.shape)
-            dark_cube=np.rollaxis(np.dstack((dark_cube,data)), 2)
+            dark_cube = np.rollaxis(np.dstack((dark_cube, data)), 2)
 
     if len(np.where(np.isnan(dark_cube))[0]):
         print("Found NaNs: "+str(np.where(np.isnan(dark_cube))))
-        dark_cube=np.where(np.isnan(dark_cube),0,dark_cube)
+        dark_cube = np.where(np.isnan(dark_cube), 0, dark_cube)
 
-    dprint(d>2, "dark_cube.shape "+str(dark_cube.shape))
-    dark_cube=dark_cube*1.
-    if len(dark_cube.shape)==3:
-        dark=median(dark_cube, axis=0)
-    elif len(dark_cube.shape)==2:
-        dark=dark_cube
+    dprint(d > 2, "dark_cube.shape " + str(dark_cube.shape))
+    dark_cube = dark_cube*1.
+    if len(dark_cube.shape) == 3:
+        dark = median(dark_cube, axis=0)
+    elif len(dark_cube.shape) == 2:
+        dark = dark_cube
     del dark_cube
 
     if sphere:
-        dark=dark*mask_nan
-    
-    bad_pix,comments = gen_badpix(dark, coef, comments, cut)
+        dark = dark*mask_nan
 
-    badpix_map=np.zeros((np.shape(dark)[0],np.shape(dark)[1]))
-    badpix_map[bad_pix]=1
-    hdulist=pyfits.PrimaryHDU()
+    bad_pix, comments = gen_badpix(dark, coef, comments, cut)
+
+    badpix_map = np.zeros((np.shape(dark)[0], np.shape(dark)[1]))
+    badpix_map[bad_pix] = 1
+    hdulist = pyfits.PrimaryHDU()
     hdr_badpix = hdulist.header
     hdr_badpix['dark'] = comments[0]
-    
+
     if flat_filename and sphere:
-        flatfield=pyfits.getdata(flat_filename, header=False)
-        flatfield=flatfield*mask_nan
-        badpix_flat,comments = gen_badpix(flatfield, coef, comments, cut)
-        badpix_map_flat=np.zeros((np.shape(flatfield)[0],np.shape(flatfield)[1]))
-        badpix_map_flat[badpix_flat]=1
-        badpix_map=badpix_map+badpix_map_flat
-        badpix_map=np.where(badpix_map>1,1,badpix_map)
-        bad_pix=tuple(np.where(badpix_map==1))
-        #bad_pix=tuple(numpy.append(numpy.array(badpix),numpy.array(badpix_flat), axis=1))
+        flatfield = pyfits.getdata(flat_filename, header=False)
+        flatfield = flatfield*mask_nan
+        badpix_flat, comments = gen_badpix(flatfield, coef, comments, cut)
+        badpix_map_flat = np.zeros((
+                np.shape(flatfield)[0], np.shape(flatfield)[1]))
+        badpix_map_flat[badpix_flat] = 1
+        badpix_map = badpix_map + badpix_map_flat
+        badpix_map = np.where(badpix_map > 1, 1, badpix_map)
+        bad_pix = tuple(np.where(badpix_map == 1))
+        # bad_pix=tuple(numpy.append(numpy.array(badpix),numpy.array(badpix_flat), axis=1))
         hdr_badpix['flat'] = comments[1]
 
-    pyfits.writeto("badpixel_map.fits",badpix_map,header=hdr_badpix,clobber=True)
+    pyfits.writeto("badpixel_map.fits", badpix_map, header=hdr_badpix,
+                   clobber=True)
     comm.bcast(bad_pix, root=0)
 
-if not rank==0:
-    dirlist=comm.recv(source = 0)
+if not rank == 0:
+    dirlist = comm.recv(source=0)
     if dirlist is None:
         print('Received None dirlist. Leaving...')
-        bad_pix=comm.bcast(None, root=0)
+        bad_pix = comm.bcast(None, root=0)
         sys.exit(1)
 
-    start=int(comm.recv(source = 0))
-    bad_pix=comm.bcast(None, root=0)
-    dprint(d>2, 'Received dirlist, start, and bad_pix')
+    start = int(comm.recv(source=0))
+    bad_pix = comm.bcast(None, root=0)
+    dprint(d > 2, 'Received dirlist, start, and bad_pix')
 
 
 t0 = MPI.Wtime()
