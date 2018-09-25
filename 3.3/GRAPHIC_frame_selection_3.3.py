014dcaf66ebba058b34bbfaaab5fa6877ac9441e
Janis Hagelberg <astro.janis@gmail.com>
2018-09-25 11:16:52 +0200

    Removed unused MPI code in GRAPHIC_frame_selection_3.3.py

--- 3.3/GRAPHIC_frame_selection_3.3.py
+++ 3.3/GRAPHIC_frame_selection_3.3.py
@@ -19,18 +19,11 @@ import numpy as np
 import graphic_nompi_lib_330 as graphic_nompi_lib
 import graphic_mpi_lib_330 as graphic_mpi_lib
 from scipy import ndimage
-from mpi4py import MPI
 import argparse
 from graphic_mpi_lib_330 import dprint
 import astropy.io.fits as pyfits
 
 import bottleneck
-
-
-nprocs = MPI.COMM_WORLD.Get_size()
-rank   = MPI.COMM_WORLD.Get_rank()
-procnm = MPI.Get_processor_name()
-comm = MPI.COMM_WORLD
 
 
 parser = argparse.ArgumentParser(description='Creates cubes with less frames by median-combining frames.')
@@ -71,169 +64,168 @@ header_keys=['frame_number', 'psf_barycentre_x', 'psf_barycentre_y', 'psf_pixel_
 target_dir = "."
 
 # Test the graphic frame rejection code:
-if rank == 0 :
-    print(sys.argv[0]+' started on '+ time.strftime("%c"))
-    hdr=None
+print(sys.argv[0]+' started on '+ time.strftime("%c"))
+hdr=None
 
-    dirlist=graphic_nompi_lib.create_dirlist(pattern)
+dirlist=graphic_nompi_lib.create_dirlist(pattern)
 
-    infolist=glob.glob(info_dir+os.sep+info_pattern+'*.rdb')
-    infolist.sort() # Sort the list alphabetically
-
-
-    cube_list,dirlist=graphic_nompi_lib.create_megatable(dirlist,infolist,keys=header_keys,nici=nici,fit=fit)
-
-    ncubes=len(dirlist)        
-        
-    # Loop through the cubes and stack the values so we can compare them all
-    valid_frames=[]
-    xcens = []
-    ycens = []
-    fluxes = []
-    xwidths = []
-    ywidths = []
-    cube_frames = []
-    frame_ix = 0
-    
-    for cube_ix in range(ncubes):
-        
-        # This containts of all the information for this cube        
-        cube_info=cube_list['info'][cube_ix]
-        
-        # How many frames are there in this cube:
-        nframes=cube_info.shape[0]
-        
-        # Make an array to keep track of the frames we want to keep
-        # Ignore already ignored frames (which should have columns 1-8 set to 1)
-        valid_frames.extend(cube_info[:,1] != -1)
-        
-        # Record the information
-        # First, get the centre positions
-        if fit:
-            xcen=cube_info[:,4]
-            ycen=cube_info[:,5]
-        else:
-            xcen=cube_info[:,1]
-            ycen=cube_info[:,2]
-        
-        xcens.extend(xcen)
-        ycens.extend(ycen)
-        
-        # Fluxes
-        fluxes.extend(cube_info[:,6])
-        
-        # PSF widths
-        xwidths.extend(cube_info[:,7])
-        ywidths.extend(cube_info[:,8])
-        
-        # And record the indices of the start and end of this cube
-        cube_frames.append(np.arange(frame_ix,frame_ix+nframes))
-        frame_ix+=nframes
-        
-    # Make everything into numpy arrays ?
-    valid_frames = np.array(valid_frames)
-    xcens = np.array(xcens)
-    ycens = np.array(ycens)
-    fluxes = np.array(fluxes)
-    xwidths = np.array(xwidths)
-    ywidths = np.array(ywidths)
-    
-    # Make some arrays to track the invalid frames
-    n_invalid = np.zeros((ncubes,4))
-    
-    # Count the initially invalid frames
-    n_invalid[:,0] = [np.sum(valid_frames[these_frames] == False) for these_frames in cube_frames]
-    
-    # Now make the cuts
-    #################
-    # SELECTION 1 : Centering
-    #################
-
-    # Unfortunately if the data is dithered we have to do it cube-by-cube
-    if dithered:
-        for cube_ix in range(ncubes):
-            
-            # Calculate the scatter in the centre positions and ignore anything far away
-            xcen_sigma=np.median(np.abs(xcens[cube_frames[cube_ix]]-np.median(xcens[cube_frames[cube_ix]])))
-            ycen_sigma=np.median(np.abs(ycens[cube_frames[cube_ix]]-np.median(ycens[cube_frames[cube_ix]])))
-            
-            xcen_diff=np.abs(xcens[cube_frames[cube_ix]]-np.median(xcens[cube_frames[cube_ix]]))
-            ycen_diff=np.abs(ycens[cube_frames[cube_ix]]-np.median(ycens[cube_frames[cube_ix]]))
-                        
-            valid_frames[cube_frames[cube_ix][xcen_diff > (centering_nsigma*xcen_sigma)]]=False
-            valid_frames[cube_frames[cube_ix][ycen_diff > (centering_nsigma*ycen_sigma)]]=False
-    else:        
-    
-        # Calculate the scatter in the centre positions and ignore anything far away
-        xcen_sigma=np.median(np.abs(xcens-np.median(xcens)))
-        ycen_sigma=np.median(np.abs(ycens-np.median(ycens)))
-        
-        xcen_diff=np.abs(xcens-np.median(xcens))
-        ycen_diff=np.abs(ycens-np.median(ycens))
-        
-        valid_frames[xcen_diff > (centering_nsigma*xcen_sigma)]=False
-        valid_frames[ycen_diff > (centering_nsigma*ycen_sigma)]=False
-    
-    # Count the invalid frames due to centring
-    n_invalid[:,1] -= np.sum(n_invalid,axis=1) # first subtract the already known ones
-    n_invalid[:,1] += [np.sum(valid_frames[these_frames] == False) for these_frames in cube_frames]
+infolist=glob.glob(info_dir+os.sep+info_pattern+'*.rdb')
+infolist.sort() # Sort the list alphabetically
 
 
-    #################
-    # SELECTION 2 : Flux (only for fitted data)
-    #################
+cube_list,dirlist=graphic_nompi_lib.create_megatable(dirlist,infolist,keys=header_keys,nici=nici,fit=fit)
+
+ncubes=len(dirlist)
+
+# Loop through the cubes and stack the values so we can compare them all
+valid_frames=[]
+xcens = []
+ycens = []
+fluxes = []
+xwidths = []
+ywidths = []
+cube_frames = []
+frame_ix = 0
+
+for cube_ix in range(ncubes):
+
+    # This containts of all the information for this cube
+    cube_info=cube_list['info'][cube_ix]
+
+    # How many frames are there in this cube:
+    nframes=cube_info.shape[0]
+
+    # Make an array to keep track of the frames we want to keep
+    # Ignore already ignored frames (which should have columns 1-8 set to 1)
+    valid_frames.extend(cube_info[:,1] != -1)
+
+    # Record the information
+    # First, get the centre positions
     if fit:
-        
-        # Calculate the scatter in the flux and ignore anything far away
-        flux_med = np.median(fluxes)
-        flux_sigma=np.median(np.abs(fluxes-flux_med))
-        flux_diff=np.abs(fluxes-flux_med)
-        valid_frames[flux_diff > (flux_nsigma*flux_sigma)]=False
-        
-    # Count the invalid frames due to flux
-    n_invalid[:,2] -= np.sum(n_invalid,axis=1) # first subtract the already known ones
-    n_invalid[:,2] += [np.sum(valid_frames[these_frames] == False) for these_frames in cube_frames]
-    
-    #################
-    # SELECTION 3 : PSF width (only for fitted data)
-    #################
-    # First, get the widths
-    if fit:
-    
-        # Calculate the scatter in the psf widths and ignore anything far away
-        xwidth_med = np.median(xwidths)
-        ywidth_med = np.median(ywidths)
-        xwidth_sigma=np.median(np.abs(xwidths-xwidth_med))
-        ywidth_sigma=np.median(np.abs(ywidths-np.median(ywidths)))
-        
-        xwidth_diff=np.abs(xwidths-xwidth_med)
-        ywidth_diff=np.abs(ywidths-np.median(ywidths))
-        
-        valid_frames[xwidth_diff > (psf_width_nsigma*xwidth_sigma)]=False
-        valid_frames[ywidth_diff > (psf_width_nsigma*ywidth_sigma)]=False
-        
+        xcen=cube_info[:,4]
+        ycen=cube_info[:,5]
     else:
-        print("  Not using fitted values. Ignoring flux and psf width!")
-        
-    # Count the invalid frames due to psf width
-    n_invalid[:,3] -= np.sum(n_invalid,axis=1) # first subtract the already known ones
-    n_invalid[:,3] += [np.sum(valid_frames[these_frames] == False) for these_frames in cube_frames]      
+        xcen=cube_info[:,1]
+        ycen=cube_info[:,2]
 
-    # Now loop through the cubes again and record the results
+    xcens.extend(xcen)
+    ycens.extend(ycen)
+
+    # Fluxes
+    fluxes.extend(cube_info[:,6])
+
+    # PSF widths
+    xwidths.extend(cube_info[:,7])
+    ywidths.extend(cube_info[:,8])
+
+    # And record the indices of the start and end of this cube
+    cube_frames.append(np.arange(frame_ix,frame_ix+nframes))
+    frame_ix+=nframes
+
+# Make everything into numpy arrays ?
+valid_frames = np.array(valid_frames)
+xcens = np.array(xcens)
+ycens = np.array(ycens)
+fluxes = np.array(fluxes)
+xwidths = np.array(xwidths)
+ywidths = np.array(ywidths)
+
+# Make some arrays to track the invalid frames
+n_invalid = np.zeros((ncubes,4))
+
+# Count the initially invalid frames
+n_invalid[:,0] = [np.sum(valid_frames[these_frames] == False) for these_frames in cube_frames]
+
+# Now make the cuts
+#################
+# SELECTION 1 : Centering
+#################
+
+# Unfortunately if the data is dithered we have to do it cube-by-cube
+if dithered:
     for cube_ix in range(ncubes):
-        
-        # Calculate how many frames were removed:
-        print "  Cube:",cube_ix,": ",np.sum(n_invalid[cube_ix])," invalid frames"
-        print_string = "    Breakdown: (initial, centering, flux, width): "
-        graphic_mpi_lib.dprint(True,print_string+str(n_invalid[cube_ix]))
-        
-        #################
-        # Update the rdb file based on the frame selection
-        #################
-        # Load it again
-        cube_info=cube_list['info'][cube_ix]
-        cube_info[valid_frames[cube_frames[cube_ix]]==False,1:]=-1
-        info_filename='all_info_framesel_'+cube_list['cube_filename'][cube_ix].replace('.fits','.rdb')
-        graphic_nompi_lib.write_array2rdb(info_dir+os.sep+info_filename,cube_info,header_keys)
-        
+
+        # Calculate the scatter in the centre positions and ignore anything far away
+        xcen_sigma=np.median(np.abs(xcens[cube_frames[cube_ix]]-np.median(xcens[cube_frames[cube_ix]])))
+        ycen_sigma=np.median(np.abs(ycens[cube_frames[cube_ix]]-np.median(ycens[cube_frames[cube_ix]])))
+
+        xcen_diff=np.abs(xcens[cube_frames[cube_ix]]-np.median(xcens[cube_frames[cube_ix]]))
+        ycen_diff=np.abs(ycens[cube_frames[cube_ix]]-np.median(ycens[cube_frames[cube_ix]]))
+
+        valid_frames[cube_frames[cube_ix][xcen_diff > (centering_nsigma*xcen_sigma)]]=False
+        valid_frames[cube_frames[cube_ix][ycen_diff > (centering_nsigma*ycen_sigma)]]=False
+else:
+
+    # Calculate the scatter in the centre positions and ignore anything far away
+    xcen_sigma=np.median(np.abs(xcens-np.median(xcens)))
+    ycen_sigma=np.median(np.abs(ycens-np.median(ycens)))
+
+    xcen_diff=np.abs(xcens-np.median(xcens))
+    ycen_diff=np.abs(ycens-np.median(ycens))
+
+    valid_frames[xcen_diff > (centering_nsigma*xcen_sigma)]=False
+    valid_frames[ycen_diff > (centering_nsigma*ycen_sigma)]=False
+
+# Count the invalid frames due to centring
+n_invalid[:,1] -= np.sum(n_invalid,axis=1) # first subtract the already known ones
+n_invalid[:,1] += [np.sum(valid_frames[these_frames] == False) for these_frames in cube_frames]
+
+
+#################
+# SELECTION 2 : Flux (only for fitted data)
+#################
+if fit:
+
+    # Calculate the scatter in the flux and ignore anything far away
+    flux_med = np.median(fluxes)
+    flux_sigma=np.median(np.abs(fluxes-flux_med))
+    flux_diff=np.abs(fluxes-flux_med)
+    valid_frames[flux_diff > (flux_nsigma*flux_sigma)]=False
+
+# Count the invalid frames due to flux
+n_invalid[:,2] -= np.sum(n_invalid,axis=1) # first subtract the already known ones
+n_invalid[:,2] += [np.sum(valid_frames[these_frames] == False) for these_frames in cube_frames]
+
+#################
+# SELECTION 3 : PSF width (only for fitted data)
+#################
+# First, get the widths
+if fit:
+
+    # Calculate the scatter in the psf widths and ignore anything far away
+    xwidth_med = np.median(xwidths)
+    ywidth_med = np.median(ywidths)
+    xwidth_sigma=np.median(np.abs(xwidths-xwidth_med))
+    ywidth_sigma=np.median(np.abs(ywidths-np.median(ywidths)))
+
+    xwidth_diff=np.abs(xwidths-xwidth_med)
+    ywidth_diff=np.abs(ywidths-np.median(ywidths))
+
+    valid_frames[xwidth_diff > (psf_width_nsigma*xwidth_sigma)]=False
+    valid_frames[ywidth_diff > (psf_width_nsigma*ywidth_sigma)]=False
+
+else:
+    print("  Not using fitted values. Ignoring flux and psf width!")
+
+# Count the invalid frames due to psf width
+n_invalid[:,3] -= np.sum(n_invalid,axis=1) # first subtract the already known ones
+n_invalid[:,3] += [np.sum(valid_frames[these_frames] == False) for these_frames in cube_frames]
+
+# Now loop through the cubes again and record the results
+for cube_ix in range(ncubes):
+
+    # Calculate how many frames were removed:
+    print("  Cube:",cube_ix,": ",np.sum(n_invalid[cube_ix])," invalid frames")
+    print_string = "    Breakdown: (initial, centering, flux, width): "
+    graphic_mpi_lib.dprint(True,print_string+str(n_invalid[cube_ix]))
+
+    #################
+    # Update the rdb file based on the frame selection
+    #################
+    # Load it again
+    cube_info=cube_list['info'][cube_ix]
+    cube_info[valid_frames[cube_frames[cube_ix]]==False,1:]=-1
+    info_filename='all_info_framesel_'+cube_list['cube_filename'][cube_ix].replace('.fits','.rdb')
+    graphic_nompi_lib.write_array2rdb(info_dir+os.sep+info_filename,cube_info,header_keys)
+
 sys.exit(0)
